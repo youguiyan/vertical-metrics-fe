aa622a2edbfd753e53c560329e690a5a98c90b77
gaohailang <ghlndsl@126.com>
2014-12-15 15:41:25 +0800

    chore: extra common code from corp into index.js~(idxCtrl, helper function etc)

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,6 +5,22 @@
 /*
     策略： 直接利用 jQuery 的插件， 在必要的时候，去取值和监听值变化驱动 UI 改变等~
  */
+function getPrevDay() {
+    var x = new Date().getTime();
+    return new Date(x - 1000 * 3600 * 24);
+}
+
+function getPrevMonday() {
+    return moment().subtract(new Date().getDay() + 6, 'days').toDate();
+}
+
+function getPrevMonth() {
+    var x = new Date();
+    x.setDate(1);
+    x.setMonth(x.getMonth() - 1);
+    return x;
+}
+
 // retentionData ? metricid = 72 & timespan = 10080 & dateTime = 20141124
 window.APIPREFIX = 'http://apps-datatools0-bgp0.hy01.wandoujia.com:8000/';
 _sparkHeight = 140;
@@ -94,20 +110,23 @@ app.filter('fakeCell', function($filter) {
 
 app.directive('sparkline', function() {
     return {
+        scope: {
+            data: '='
+        },
         link: function(scope, element, attrs) {
-            var data = _.map(_.range(1, 10), function(i) {
+            var data = scope.data || _.map(_.range(1, 10), function(i) {
                 return [i, _.random(20, 100)];
             });
             $(element).sparkline(data, {
-                width: (+attrs.width) || 200, //Width of the chart - Defaults to 'auto' - May be any valid css width - 1.5em, 20px, etc (using a number without a unit specifier won't do what you want) - This option does nothing for bar and tristate chars (see barWidth)
-                height: (+attrs.height) || 40, //Height of the chart - Defaults to 'auto' (line height of the containing tag)
-                lineColor: '#2FABE9', //Used by line and discrete charts to specify the colour of the line drawn as a CSS values string
-                fillColor: '#f2f7f9', //Specify the colour used to fill the area under the graph as a CSS value. Set to false to disable fill
-                spotColor: '#467e8c', //The CSS colour of the final value marker. Set to false or an empty string to hide it
-                maxSpotColor: '#b9e672', //The CSS colour of the marker displayed for the maximum value. Set to false or an empty string to hide it
-                minSpotColor: '#FA5833', //The CSS colour of the marker displayed for the mimum value. Set to false or an empty string to hide it
-                spotRadius: 2, //Radius of all spot markers, In pixels (default: 1.5) - Integer
-                lineWidth: 1 //In pixels (default: 1) - Integer
+                width: (+attrs.width) || 200,
+                height: (+attrs.height) || 40,
+                lineColor: '#2FABE9',
+                fillColor: '#f2f7f9',
+                spotColor: '#467e8c',
+                maxSpotColor: '#b9e672',
+                minSpotColor: '#FA5833',
+                spotRadius: 2,
+                lineWidth: 1
             });
         }
     }
@@ -135,3 +154,99 @@ app.filter('percentize', ['$filter',
         };
     }
 ]);
+
+app.controller('idxCtrl', function($scope, $rootScope, $filter) {
+    // fake states
+    $scope.dateTime = $filter('date')(getPrevDay(), 'yyyyMMdd');
+    $scope.timespan = '1440'; // default day mode
+
+    $scope.getBaseVm = function() {
+        return _.pick($scope, 'dateTime', 'timespan');
+    };
+
+    $scope.getBaseDimension = function() {
+        return $scope.dimension;
+    };
+
+    // $on 'dimensionChanged', 'timespan', 'dateTime'
+    $scope.$watch('dateTime', function(v) {
+        if (!v) return;
+        $scope.$broadcast('baseDateTimeChanged', {});
+    });
+
+    // set datepicker jquery plugin
+    $scope.$watch('timespan', function(timespan) {
+        if (!timespan) return;
+        var $datePicker = $('.date-picker');
+        var baseOpt = {
+            autoclose: true,
+            todayHighlight: true,
+            endDate: new Date()
+        };
+        var timespanDPoptionMap = {
+            1440: _.extend({}, baseOpt, {
+                format: 'yyyy-mm-dd'
+            }),
+            10080: _.extend({}, baseOpt, {
+                format: 'yyyy-mm-dd',
+                daysOfWeekDisabled: "0,2,3,4,5,6"
+            }),
+            43200: _.extend({}, baseOpt, {
+                format: 'yyyy-mm',
+                minViewMode: 'months',
+                startView: 'months'
+            })
+        };
+        var prevFnMap = {
+            1440: getPrevDay,
+            10080: getPrevMonday,
+            43200: getPrevMonth
+        };
+        $datePicker.datepicker('remove');
+        $datePicker.datepicker(timespanDPoptionMap[timespan]).on('changeDate', function(e) {
+            $scope.dateTime = e.format().replace(/-/g, '');
+            if (!$scope.$$phase) {
+                //$digest or $apply
+                $scope.$apply();
+            }
+        });
+        $datePicker.datepicker('setDate', prevFnMap[timespan].call());
+        $scope.$broadcast('baseTimeSpanChanged', {});
+    }, true);
+
+    // slider ui for dimension select
+    $scope.$watch('dimension', function(v) {
+        if (!v) return;
+        $rootScope.$broadcast('dimensionChanged', v);
+    }, true);
+    $scope.dimension = {
+        isCheat: '', // default all value
+        isNewUser: ''
+    };
+    $scope.isCheatOptions = [
+        ['0', '非作弊'],
+        ['1', '作弊'],
+        ['', '全部']
+    ];
+    $scope.isNewUserOptions = [
+        ['0', '老用户'],
+        ['1', '新用户'],
+        ['', '全部']
+    ];
+    $scope.timespanOptions = [
+        ['1440', '天'],
+        ['10080', '周']
+    ];
+
+    $scope.metricList = [];
+    // click metric card to toggle select status
+    $scope.toggleSelect = function(i) {
+        i.selected = !i.selected;
+    };
+    $scope.$watch('metricList', function(v) {
+        if (!v) return;
+        $rootScope.$broadcast('selectedMetricsChange', _.pluck(_.filter(v, function(v) {
+            return v.selected;
+        }), 'metricid'));
+    }, true);
+});
