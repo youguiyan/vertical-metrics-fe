971e930281b1d096084582db82123fdf7c0b2a58
gaohailang <ghlndsl@126.com>
2014-12-31 02:04:02 +0800

    feat: make flow path render to more robust and elegant(with fold line, arrow, etc)

--- app/scripts/flow-path-render.js
+++ app/scripts/flow-path-render.js
@@ -6,6 +6,7 @@ function renderFlowPathChart(NdluData, veritical) {
     var centerCiccleR = 60;
     var maxCircleR = 80;
     var minCircleR = 30;
+    var lineSize = 15;
 
     var totalNum = _.reduce(_.values(NdluData.verticaldata), function(memo, num) {
         return memo + num;
@@ -14,9 +15,23 @@ function renderFlowPathChart(NdluData, veritical) {
     var minMetricNum = _.min(_.values(NdluData.verticaldata));
     var maxCircleR = getRatioR(maxMetricNum);
 
+    var linePointOffset = centerCiccleR / 2 * Math.sqrt(3);
+
+    var linePointMap = {
+        0: [containerWidth / 2, containerHeight / 2 - centerCiccleR],
+        1: [containerWidth / 2 - linePointOffset, containerHeight / 2 - centerCiccleR / 2],
+        2: [(containerWidth / 2) - centerCiccleR, containerHeight / 2],
+        3: [(containerWidth / 2) - centerCiccleR, containerHeight / 2],
+        4: [(containerWidth / 2) - linePointOffset, containerHeight / 2 + centerCiccleR / 2],
+        5: [containerWidth / 2, containerHeight / 2 + centerCiccleR]
+    };
 
     function getRatioR(part) {
         return (part / totalNum) * ((containerHeight - 10 * 5) / 2);
+    }
+
+    function getTextWidthOffsize(txt, ren) {
+        return ren.text(txt, -100, -100).add().getBBox().width * 0.5;
     }
     var chart = new Highcharts.Chart({
         chart: {
@@ -27,7 +42,7 @@ function renderFlowPathChart(NdluData, veritical) {
                     // Draw the flow chart
                     var ren = this.renderer,
                         colors = Highcharts.getOptions().colors,
-                        rightArrow = ['M', 0, 0, 'L', 100, 0, 'L', 95, 5, 'M', 100, 0, 'L', 95, -5],
+                        rightArrow = ['M', 0, 0, 'L', 10, 0, 'L', 5, 5, 'M', 10, 0, 'L', 5, -5],
                         leftArrow = ['M', 100, 0, 'L', 0, 0, 'L', 5, 5, 'M', 0, 0, 'L', 5, -5];
 
                     // circle @ center
@@ -37,41 +52,48 @@ function renderFlowPathChart(NdluData, veritical) {
                         'stroke-width': 1
                     }).add();
 
-                    ren.label(veritical + '<br>New User<br>' + totalNum, containerWidth / 2, containerHeight / 2).css({
+                    var _centerLabel = veritical + '<br>' + totalNum;
+                    ren.label(_centerLabel, containerWidth / 2 - getTextWidthOffsize(_centerLabel, ren), containerHeight / 2 - lineSize).css({
                         fontWeight: 'bold'
                     }).add();
 
                     var lastCircleY = 0;
                     var i = 0;
+                    var _leftLabel;
                     _.each(NdluData.verticaldata, function(val, key) {
                         var r = getRatioR(val);
+                        // left circle
                         ren.circle(maxCircleR, lastCircleY + r, r).attr({
                             fill: colors[i],
                             stroke: '#fff',
                             'stroke-width': 1
                         }).add();
-                        ren.label(key + '<br>' + val, maxCircleR, lastCircleY + r).css({
+
+                        _leftLabel = key + '<br>' + val;
+                        ren.label(_leftLabel, maxCircleR - getTextWidthOffsize(_leftLabel, ren), lastCircleY + r - lineSize).css({
                             fontWeight: 'bold'
                         }).add();
 
-                        ren.path(['M', maxCircleR + r, lastCircleY + r, 'L', containerWidth / 2, containerHeight / 2]).attr({
+                        // line - circleR
+                        ren.path(['M', maxCircleR + r, lastCircleY + r, 'L', linePointMap[i][0], lastCircleY + r, 'L', linePointMap[i][0], linePointMap[i][1]]).attr({
                             'stroke-width': 2,
                             stroke: 'silver',
                             dashstyle: 'dash'
-                        })
-                            .add();
-
-                        ren.label(NdluData.verticalbringdata[key], (maxCircleR + r + containerWidth / 2) / 2, (lastCircleY + r + containerHeight / 2) / 2).css({
-                            fontWeight: 'bold'
                         }).add();
 
-                        /*ren.path(rightArrow)
-                             .attr({
-                                 'stroke-width': 2,
-                                 stroke: colors[1]
-                             })
-                            .translate((maxCircleR+r+containerWidth/2)/1.5, (lastCircleY+r+containerHeight/2)/1.5)
-                             .add();*/
+                        // arrow for line
+                        ren.path(rightArrow)
+                            .attr({
+                                'stroke-width': 2,
+                                stroke: colors[i]
+                            })
+                            .translate((maxCircleR + r + containerWidth / 2) / 2.5, lastCircleY + r)
+                            .add();
+
+                        // 数字@line
+                        ren.label(NdluData.verticalbringdata[key], (maxCircleR + r + containerWidth / 2) / 2, lastCircleY + r - lineSize).css({
+                            fontWeight: 'bold'
+                        }).add();
 
                         lastCircleY += r * 2 + circlePadding;
                         i += 1;
@@ -80,7 +102,10 @@ function renderFlowPathChart(NdluData, veritical) {
             }
         },
         exporting: {
-            enabled: true
+            enabled: false
+        },
+        credits: {
+            enabled: false
         },
         title: {
             text: ''
